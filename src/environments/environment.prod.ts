dfc38e431da4c933ef0d1ab84e881342824771b7
ChenBexiuan <percyquispe88@gmail.com>
2025-09-13 10:50:34 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'api-libreria-bwb6hbfhc7h4b7bm.canadacentral-01.azurewebsites.net/api'
+  apiUrl: 'https://api-libreria-bwb6hbfhc7h4b7bm.canadacentral-01.azurewebsites.net/api'
 };
