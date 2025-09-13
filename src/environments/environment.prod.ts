8567db40363e6b0314f45fe60187d0e45a9f9b94
Mdro123 <134395516+Mdro123@users.noreply.github.com>
2025-09-12 23:10:18 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'api-libreria-bwb6hbfhc7h4b7bm.canadacentral-01.azurewebsites.net'
+  apiUrl: 'api-libreria-bwb6hbfhc7h4b7bm.canadacentral-01.azurewebsites.net/api'
 };
